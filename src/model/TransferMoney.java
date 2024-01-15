7a04513e7cb01892c7790d3bb82596327da87116
Admin <funedy@yandex.ru>
2024-01-15 22:40:52 +0300

    fix paths

--- src/model/TransferMoney.java
+++ src/model/TransferMoney.java
@@ -21,7 +21,7 @@ public class TransferMoney {
             if (filesRead.isEmpty()) {
                 throw new ReadFileNull("Нету отсортированных файлов");
             }
-            DirectoryCleaner.clearDirectory(Files_Path.PATH_SOURCE, "результатов");
+            DirectoryCleaner.clearDirectory(Files_Path.PATH_RESULT, "результатов");
             for (int i = 0; i < filesRead.size(); i++) {
                 int j = i + 1;
                 String[] strings = filesRead.get(i).split(" ");
@@ -34,13 +34,13 @@ public class TransferMoney {
                     }
                     int i1 = Integer.parseInt(strings[1]) - Integer.parseInt(strings[4]);
                     int i2 = Integer.parseInt(strings[3]) + Integer.parseInt(strings[4]);
-                    Path file = Files.createFile(Path.of(Files_Path.PATH_SOURCE + "files_" + j + "_" + LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + "-good.txt"));
+                    Path file = Files.createFile(Path.of(Files_Path.PATH_RESULT + "files_" + j + "_" + LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + "-good.txt"));
                     Files.writeString(file, strings[0] + " " + i1 + " " + strings[2] + " " + i2 + " " + strings[4]);
                     System.out.println("Перевод выполнен для файла - " + j);
                     ListLog.arrayList.add("Перевод выполнен для файла - " + j);
 
                 } catch (FailCount | FailFormatNumber e) {
-                    Path file = Files.createFile(Path.of(Files_Path.PATH_SOURCE + "files_" + j + "_" + LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + "-bad.txt"));
+                    Path file = Files.createFile(Path.of(Files_Path.PATH_RESULT + "files_" + j + "_" + LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + "-bad.txt"));
                     Files.writeString(file, e.getMessage());
                     System.out.println("Перевод не выполнен для файла - " + j + " Причина - " + e.getMessage());
                     ListLog.arrayList.add("Перевод не выполнен для файла - " + j + " Причина - " + e.getMessage());
